17de395193e530c80c96c28f61429b888bba404d
Tibor Bacsi <zeroxx1986@gmail.com>
2022-01-24 13:47:57 +0100

    initial commit

---
+++ data-updates.lua
@@ -0,0 +1,8 @@
+data.raw["logistic-robot"]["logistic-robot"].speed = settings.startup["enhanced-drones-logistic-robot-speed"].value
+data.raw["construction-robot"]["construction-robot"].speed = settings.startup["enhanced-drones-construction-robot-speed"].value
+
+data.raw["logistic-robot"]["logistic-robot"].energy_per_move = settings.startup["enhanced-drones-logistic-robot-energy-per-move"].value
+data.raw["construction-robot"]["construction-robot"].energy_per_move = settings.startup["enhanced-drones-construction-robot-energy-per-move"].value
+
+data.raw["logistic-robot"]["logistic-robot"].energy_per_tick = settings.startup["enhanced-drones-logistic-robot-energy-per-tick"].value
+data.raw["construction-robot"]["construction-robot"].energy_per_tick = settings.startup["enhanced-drones-construction-robot-energy-per-tick"].value
---
+++ settings.lua
@@ -0,0 +1,31 @@
+data:extend({{
+    type = "double-setting",
+    name = "enhanced-drones-logistic-robot-speed",
+    setting_type = "startup",
+    default_value = "0.05"
+}, {
+    type = "double-setting",
+    name = "enhanced-drones-construction-robot-speed",
+    setting_type = "startup",
+    default_value = "0.06"
+}, {
+    type = "string-setting",
+    name = "enhanced-drones-logistic-robot-energy-per-move",
+    setting_type = "startup",
+    default_value = "5kJ"
+}, {
+    type = "string-setting",
+    name = "enhanced-drones-construction-robot-energy-per-move",
+    setting_type = "startup",
+    default_value = "5kJ"
+}, {
+    type = "string-setting",
+    name = "enhanced-drones-logistic-robot-energy-per-tick",
+    setting_type = "startup",
+    default_value = "0.05kJ"
+}, {
+    type = "string-setting",
+    name = "enhanced-drones-construction-robot-energy-per-tick",
+    setting_type = "startup",
+    default_value = "0.05kJ"
+}})
